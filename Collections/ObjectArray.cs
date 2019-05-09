21e4b96f87b2a2325647373a23cf5e2b42b73975
BaladaRock <48443239+BaladaRock@users.noreply.github.com>
2019-05-09 16:40:10 +0300

    Tests added up to ObjectArray.cs

---
+++ Collections/ObjectArray.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace CollectionsClasses
+{
+    public class ObjectArray
+    {
+
+        protected object[] array;
+
+        public ObjectArray(int capacity = 4)
+        {
+            Count = 0;
+            array = new object[capacity];
+        }
+
+        public int Count { get; private set; }
+
+        public virtual object this[int index]
+        {
+            get => array[index];
+            set => array[index] = value;
+        }
+
+        public virtual void Add(object element)
+        {
+            EnsureCapacity();
+            array[Count++] = element;
+        }
+
+        public bool Contains(object element)
+        {
+            return IndexOf(element) != -1;
+        }
+
+        public int IndexOf(object element)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (array[i] == element
+                 ||(array[i]?.Equals(element) == true))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public virtual void Insert(int index, object element)
+        {
+            InsertElement(index, element);
+        }
+
+        private void InsertElement(int index, object element)
+        {
+            EnsureCapacity();
+            ShiftRight(index);
+            array[index] = element;
+            Count++;
+        }
+
+        public void Clear()
+        {
+            Count = 0;
+        }
+
+        public void Remove(object element)
+        {
+            RemoveAt(IndexOf(element));
+        }
+
+        public void RemoveAt(int index)
+        {
+            ShiftLeft(index);
+            Count--;
+        }
+
+        private void ShiftLeft(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+                array[i] = array[i + 1];
+        }
+
+        private void ShiftRight(int index)
+        {
+            for (int i = Count; i > index; i--)
+                array[i] = array[i - 1];
+        }
+
+        private void EnsureCapacity()
+        {
+            if (Count == array.Length)
+                Array.Resize(ref array, array.Length * 2);
+        }
+
+
+
+    }
+}
---
+++ CollectionsTests/ObjectArrayFacts.cs
@@ -0,0 +1,87 @@
+﻿using Xunit;
+using CollectionsClasses;
+
+namespace CollectionsTests
+{
+    public class ObjectArrayFacts
+    {
+        [Fact]
+        public void Test_Object_Array_Contains_String_And_Int()
+        {
+
+            var mixedArray = new ObjectArray();
+            mixedArray.Add("Andrei");
+            mixedArray.Add(1);
+
+            Assert.Equal("Andrei", mixedArray[0]);
+            Assert.Equal(2, mixedArray.Count);
+
+        }
+
+        [Fact]
+        public void Test_Set_Object_Array_of_String()
+        {
+
+            var stringArray = new ObjectArray();
+            stringArray.Add("Andrei");
+            stringArray.Add("Dota");
+
+            stringArray[0] = "eu";
+            Assert.Equal("eu", stringArray[0]);
+            Assert.Equal(2, stringArray.Count);
+
+        }
+
+        [Fact]
+        public void Test_Contains_Object_Array_of_String()
+        {
+
+            var stringArray = new ObjectArray();
+            stringArray.Add("Andrei");
+            stringArray.Add("Dota");
+
+            stringArray[0] = "eu";
+            Assert.True(stringArray.Contains("eu"));
+            Assert.False(stringArray.Contains(null));
+            Assert.Equal(2, stringArray.Count);
+
+        }
+
+        [Fact]
+        public void Test_IndexOf_Object_Array_of_Booleans()
+        {
+
+            var stringArray = new ObjectArray();
+            stringArray.Add(true);
+            stringArray.Add(false);
+
+            Assert.False(stringArray.Contains("eu"));
+            Assert.False(stringArray.Contains(null));
+            Assert.Equal(0, stringArray.IndexOf(true));
+
+        }
+
+        [Fact]
+        public void Test_Insert_Object_Mixed_Array()
+        {
+
+            var stringArray = new ObjectArray();
+            stringArray.Add(true);
+            stringArray.Add(false);
+            stringArray.Add("IT");
+            stringArray.Add(null);
+            stringArray.Add(string.Empty);
+            stringArray.Add(int.MaxValue);
+            stringArray.Insert(1, 1);
+            stringArray.Insert(4, string.Empty);
+
+            Assert.False(stringArray.Contains("eu"));
+            Assert.True(stringArray.Contains(null));
+            Assert.Equal(0, stringArray.IndexOf(true));
+            Assert.Equal(1, stringArray.IndexOf(1));
+            Assert.Equal(8, stringArray.Count);
+
+        }
+        
+    }
+}
